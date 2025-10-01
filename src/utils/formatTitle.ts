705ecc84a73a2af431df479185c5c2222e5df8d4
KungSovannda <kungsovannda@gmail.com>
2025-10-01 12:10:13 +0700

    update: to lower case

--- src/utils/formatTitle.ts
+++ src/utils/formatTitle.ts
@@ -1,6 +1,9 @@
 export function formatTitle(slug: string) {
-  return slug
-    .toLowerCase()
-    .replace(/-/g, " ")
-    .replace(/\b\w/g, (char) => char.toUpperCase());
+  return (
+    slug ||
+    ""
+      .toLowerCase()
+      .replace(/-/g, " ")
+      .replace(/\b\w/g, (char) => char.toUpperCase())
+  );
 }
