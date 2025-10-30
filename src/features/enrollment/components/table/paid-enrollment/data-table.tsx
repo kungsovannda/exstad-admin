b7cdb366484adb34208e302f194e8cd156a7c59c
KungSovannda <kungsovannda@gmail.com>
2025-10-30 12:03:28 +0700

    upate: send email

--- src/features/enrollment/components/table/paid-enrollment/data-table.tsx
+++ src/features/enrollment/components/table/paid-enrollment/data-table.tsx
@@ -4,10 +4,7 @@ import { type ColumnDef } from "@tanstack/react-table";
 
 import ExportToExcelModal from "@/components/ExportToExcelModal";
 import ModalProcess from "@/components/modal/ModalProcess";
-import {
-  generateQRCodeBase64,
-  generateQRCodeFile,
-} from "@/components/qr-code-generator";
+import { generateQRCodeBase64 } from "@/components/qr-code-generator";
 import { DataTable } from "@/components/table/data-table";
 import { DataTableToolbar } from "@/components/table/data-table-toolbar";
 import { Button } from "@/components/ui/button";
@@ -20,7 +17,6 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useDownloadApplicantLettersZipMutation } from "@/features/application/applicationApi";
-import { useCreateDocumentMutation } from "@/features/document/documentApi";
 import { useCreateEmailMessageMutation } from "@/features/email/emailApi";
 import { useUpdateEnrollmentMutation } from "@/features/enrollment/enrollmentApi";
 import { useDataTable } from "@/hooks/use-data-table";
@@ -28,12 +24,14 @@ import { useAppSelector } from "@/lib/hooks";
 import { exportToExcel } from "@/services/export-to-excel";
 import { ApplicantLetterRequest } from "@/types/application";
 import { Enrollment } from "@/types/enrollment";
+import { openingProgramType } from "@/types/opening-program";
+import { MasterProgramType } from "@/types/program";
 import { ChevronDown, Printer } from "lucide-react";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
-import { openingProgramType } from "@/types/opening-program";
-import { MasterProgramType } from "@/types/program";
+import z from "zod";
+import { SendEmailModal } from "../../SendEmailModal";
 
 interface PaidEnrollmentTableProps<TValue> {
   columns: ColumnDef<Enrollment, TValue>[];
@@ -75,6 +73,7 @@ export function PaidEnrollmentTable<TValue>({
 
   const [updateEnrollment] = useUpdateEnrollmentMutation();
   const preference = useAppSelector((state) => state.preference);
+  const [isSendEmailOpen, setIsSendEmailOpen] = useState(false);
 
   useEffect(() => {
     setIsShortCourse(searchParams.get("type") === "short-course");
@@ -203,43 +202,61 @@ export function PaidEnrollmentTable<TValue>({
 
   const [sendEmail] = useCreateEmailMessageMutation();
 
-  const onSendLetterHandle = () => {
+  type schema = {
+    subject: string;
+    message: string;
+    examDate: string;
+    examTime: string;
+    location: string;
+  };
+
+  const onSendLetterHandle = async ({
+    message,
+    subject,
+    examDate,
+    examTime,
+    location,
+  }: schema) => {
     const enrollments = table.getSelectedRowModel().rows.map((r) => r.original);
 
-    enrollments.forEach(async (enrollment) => {
-      const qrFile = await generateQRCodeBase64({
-        text: `https://admin.exstad.tech/enrollment/${program?.slug}/check-in?id=${enrollment.uuid}`,
-        size: 2000,
-        dotsColor: "#4F46E5",
-        backgroundColor: "#ffffff",
-        dotsType: "rounded",
-        logoUrl: "https://www.exstad.tech/image/logo/exSTAD-01.png",
-        logoSize: 0.3,
-        logoMargin: 15,
-      });
+    for (const enrollment of enrollments) {
+      try {
+        const qrFile = await generateQRCodeBase64({
+          text: `https://admin.exstad.tech/enrollment/${program?.slug}/check-in?id=${enrollment.uuid}`,
+          size: 2000,
+          dotsColor: "#4F46E5",
+          backgroundColor: "#ffffff",
+          dotsType: "rounded",
+          logoUrl: "https://www.exstad.tech/image/logo/exSTAD-01.png",
+          logoSize: 0.3,
+          logoMargin: 15,
+        });
 
-      toast.promise(
-        sendEmail({
-          name: enrollment.englishName,
-          email: enrollment.email,
-          subject: "Admission Letter from EXSTAD",
-          message: "Below are your admission details.",
-          toEmail: enrollment.email,
-          admissionLetterUrl: enrollment.applicantLetter,
-          qrCodeFile: qrFile,
-          examDetails: {
-            date: "2025-01-15",
-            time: "10:00 AM",
-            location: "Phnom Penh Exam Center",
-          },
-        }).unwrap(),
-        {
-          loading: `Sending email to ${enrollment.email}...`,
-          success: `Email sent to ${enrollment.email}!`,
-          error: `Failed to send email to ${enrollment.email}.`,
-        }
-      );
-    });
+        toast.promise(
+          sendEmail({
+            name: enrollment.englishName,
+            email: "info.exstad@gmail.com",
+            subject,
+            message,
+            toEmail: enrollment.email,
+            admissionLetterUrl: enrollment.applicantLetter,
+            qrCodeFile: qrFile,
+            examDetails: {
+              date: examDate,
+              time: examTime,
+              location,
+            },
+          }).unwrap(),
+          {
+            loading: `Sending email to ${enrollment.email}...`,
+            success: `Email sent to ${enrollment.email}!`,
+            error: `Failed to send email to ${enrollment.email}.`,
+          }
+        );
+      } catch (error) {
+        console.error("Failed to send email:", error);
+      }
+    }
   };
 
   return (
@@ -289,7 +306,7 @@ export function PaidEnrollmentTable<TValue>({
               <DropdownMenuItem onClick={onGenerateApplicationLetter}>
                 Application Letter
               </DropdownMenuItem>
-              <DropdownMenuItem onClick={onSendLetterHandle}>
+              <DropdownMenuItem onClick={() => setIsSendEmailOpen(true)}>
                 Send Letter
               </DropdownMenuItem>
             </DropdownMenuContent>
@@ -323,6 +340,14 @@ export function PaidEnrollmentTable<TValue>({
           successMsg="enrollments marked as interviewed"
           failMsg="fail to mark enrollment"
           total={table.getSelectedRowModel().rows.length}
+        />
+      )}
+      {isSendEmailOpen && (
+        <SendEmailModal
+          open={isSendEmailOpen}
+          onOpenChange={setIsSendEmailOpen}
+          openingProgram={openingProgram!}
+          onSubmitForm={onSendLetterHandle}
         />
       )}
     </DataTable>
